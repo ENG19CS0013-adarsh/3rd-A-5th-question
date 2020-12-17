fee51346d4c91216a3dc9f6bef3afbf296a77249
ENG19CS0013-adarsh <76088953+ENG19CS0013-adarsh@users.noreply.github.com>
2020-12-17 14:01:08 +0530

    Update and rename debug_exam.py to debug_exam.solution.py

--- debug_exam.solution.py
+++ debug_exam.solution.py
@@ -112,3 +112,46 @@ if __name__ == '__main__':
     print(data1)
     print(data2)
     print(dup)
+
+'''
+5)a) Input 
+    n1=3
+    1 2
+    2 2
+    9 4
+    n2=2
+    3 3
+    1 4
+    expected output 
+    {2:2,9:4,3:3}
+    [[3,3],[1,4]]
+    {1:[2,4]}
+    
+    5)b)
+    in line 53 and 55 there was an indentation error. The else block belongs to the outer if and not the inner if block.
+    
+5)c)
+(I)Input 
+n1=3
+1 2
+2 2
+9 4
+n2=2
+3 3
+1 4
+expected output
+{2:2,9:4,3:3}
+[[3,3],[1,4]]
+{1:[2,4]}
+(II)
+n1=2
+1 2
+2 4
+n2=2
+3 4
+2 5
+expected output
+{2:4,3:4}
+[[3,4],[2,5]]
+{2:[4,5]}
+
